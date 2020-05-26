f7e2584a2b7539f22eca6317326f54cdd903c556
Yufeng-Liu <51807210+Yufeng-Liu@users.noreply.github.com>
2020-05-26 23:36:39 +0800

    Update 1.c

--- liuyufeng/1.c
+++ liuyufeng/1.c
@@ -1,4 +1,11 @@
 /**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+解题思路：（1）两个for循环
+（2）先排序，用二分法查找
+（3）用harsh列表，用target值减去扫描初始值，然后与链表内值做比较。
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
