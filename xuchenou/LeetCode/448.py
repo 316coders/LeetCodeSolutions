a4a98b2b7fba8edbe1b205473ebc696bc2b14480
徐晨鸥 <464154015@qq.com>
2020-06-22 16:31:55 +0800

    Create 448.py

---
+++ xuchenou/LeetCode/448.py
@@ -0,0 +1,63 @@
+"""
+448. 找到所有数组中消失的数字
+给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+
+找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+
+您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+
+示例:
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[5,6]
+"""
+from typing import List
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        # Iterate over each of the elements in the original array
+        for i in range(len(nums)):
+            
+            # Treat the value as the new index
+            new_index = abs(nums[i]) - 1
+            
+            # Check the magnitude of value at this new index
+            # If the magnitude is positive, make it negative 
+            # thus indicating that the number nums[i] has 
+            # appeared or has been visited.
+            if nums[new_index] > 0:
+                nums[new_index] *= -1
+        
+        # Response array that would contain the missing numbers
+        result = []    
+        
+        # Iterate over the numbers from 1 to N and add all those
+        # that have positive magnitude in the array 
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)
+                
+        return result  
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         #范围在 1到n之间,刚好每一个位置一个数
+#         n = len(nums)
+#         res = [i for i in range(1,n+1)]
+#         for i in range(0,n):
+#             while(nums[i] != i+1):
+#                 temp = nums[i] - 1
+#                 nums[i],nums[temp] = nums[temp],nums[i]
+#                 res.remove(nums[temp])
+#                 if(nums[i] == nums[temp]):
+#                     break
+#         return res
+
+l1 = [4,3,2,7,8,2,3,1]
+s1 = Solution()
+print(s1.findDisappearedNumbers(l1))
